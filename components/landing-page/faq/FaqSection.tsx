a8831b285c0a343a1b5168c0e21a0b302b53bf07
kvchi <jonathanmkpuma4@gmail.com>
2026-01-22 11:41:14 +0100

    feat: improve features section and footer layout

--- components/landing-page/faq/FaqSection.tsx
+++ components/landing-page/faq/FaqSection.tsx
@@ -3,9 +3,9 @@ import FaqList from './FaqList'
 
 export default function FaqSection() {
   return (
-    <section className="bg-background ">
+    <section className="bg-background pb-16">
       <div className="max-w-6xl mx-auto px-6">
-        <div className="text-center mb-16">
+        <div className="text-center my-16">
       <h2 className="text-3xl font-semibold mb-4">
         Frequently asked questions
       </h2>
